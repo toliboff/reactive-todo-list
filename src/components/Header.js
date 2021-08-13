ce358770704c2f41e78c74a3f3788ee81c5f43cc
Tolibjon <tolibjohn@gmail.com>
2021-08-13 20:15:44 +0500

    Create Header component

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+/* eslint-disable */
+import React from 'react';
+
+const Header = () => {
+  return (
+    <div>
+      <header>
+        <h1>todos</h1>
+      </header>
+    </div>
+  )
+}
+
+export default Header;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Header from './components/Header';
 import TodoContainer from './components/TodoContainer';
 
 ReactDOM.render(
   <React.StrictMode>
+    <Header />
     <TodoContainer />
   </React.StrictMode>,
   document.getElementById('root'),
